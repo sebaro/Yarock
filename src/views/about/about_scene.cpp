07c526760d1d1f48dc6f883ef1e742ce87062fda
sebastien amardeilh <sebastien.amardeilh@gmail.com>
2015-12-31 08:42:43 +0100

    New About scene

--- src/views/about/about_scene.cpp
+++ src/views/about/about_scene.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************************
 *  YAROCK                                                                               *
-*  Copyright (c) 2010-2015 Sebastien amardeilh <sebastien.amardeilh+yarock@gmail.com>   *
+*  Copyright (c) 2010-2016 Sebastien amardeilh <sebastien.amardeilh+yarock@gmail.com>   *
 *                                                                                       *
 *  This program is free software; you can redistribute it and/or modify it under        *
 *  the terms of the GNU General Public License as published by the Free Software        *
@@ -18,15 +18,25 @@
 #include "config.h"
 
 #include "views/item_common.h"
+#include "views/context/context_item.h"
+#include "engine.h"
 #include "debug.h"
 
 // Qt
 #include <QtCore>
 #include <QLabel>
+#include <QGraphicsView>
 #include <QGraphicsProxyWidget>
 #include <QHBoxLayout>
-#include <QFrame>
-#include <QTextBrowser>
+#include <QApplication>
+
+
+const QString S_MAIL      = "<a href=\"mailto:sebastien.amardeilh+yarock@gmail.com\">(sebastien.amardeilh+yarock@gmail.com)</a>";
+const QString S_LAUNCHPAD = "<a href=\"https://bugs.launchpad.net/yarock\">https://bugs.launchpad.net/yarock</a>";
+const QString S_TRANSLATE = "<a href=\"https://www.transifex.net/projects/p/yarock-translation/\">transifex</a>";
+const QString S_DONATE    = "<a target=”_blank” href=\"https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=PBL2A87TRZPH4\">donate</a>";
+const QString S_DONATE2   = "<a href=\"http://qt-apps.org/content/donate.php?content=129372\">other donate link</a>";
+const QString S_WEBSITE   = "<a href=\"http://seb-apps.github.io/yarock/\">website</a>";
 
 /*
 ********************************************************************************
@@ -65,107 +75,182 @@ void AboutScene::populateScene()
     Debug::debug() << "   [AboutScene] populateScene";    
     clear();
  
-    /* content page */ 
-    QWidget* main_widget = new QWidget();
-    main_widget->setAttribute(Qt::WA_NoBackground, true);
-    main_widget->setAutoFillBackground(true);
-    main_widget->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-    main_widget->setMinimumWidth(600);
-    main_widget->setMinimumHeight(600);
+    /* proxy widget content */
+      QWidget* widget1 = new QWidget();
+      widget1->setAttribute(Qt::WA_NoBackground, true);
+      widget1->setAutoFillBackground(true);
+      widget1->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
     
 
-    QLabel *title = new QLabel(main_widget);
-    title->setText(APP_NAME);
+      QLabel *title = new QLabel(widget1);
+      title->setText(APP_NAME);
 
-    QFont title_font;
-    title_font.setBold(true);
-    title_font.setPointSize(title_font.pointSize() + 4);
-    title->setFont(title_font);
+      QFont title_font;
+      title_font.setBold(true);
+      title_font.setPointSize(title_font.pointSize() + 4);
+      title->setFont(title_font);
 
-    QLabel *version = new QLabel(main_widget);
-    version->setText(VERSION);
+      QLabel *version = new QLabel(widget1);
+      version->setText(VERSION);
 
-    QLabel *copyright = new QLabel(main_widget);
-    copyright->setText("(c) 2010-2015 yarock author");
+      QLabel *copyright = new QLabel(widget1);
+      copyright->setText("(c) 2010-2015 Sebastien Amardeilh");
 
-    QLabel *icon = new QLabel(main_widget);
-    icon->setPixmap( QPixmap(":/icon/yarock_64x64.png") );
-    icon->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+      QLabel *icon = new QLabel(widget1);
+      icon->setPixmap( QPixmap(":/icon/yarock_64x64.png") );
+      icon->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
 
-    QFrame *line = new QFrame(main_widget);
-    line->setFrameShape(QFrame::HLine);
-    line->setFrameShadow(QFrame::Sunken);
-    line->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
+      QVBoxLayout * vl0 = new QVBoxLayout();
+      vl0->setContentsMargins(0,0,0,0);
+      vl0->addWidget(title);
+      vl0->addWidget(version);
+      vl0->addWidget(copyright);
 
-    QVBoxLayout * vl0 = new QVBoxLayout();
-    vl0->setContentsMargins(10,0,0,0);
-    vl0->addWidget(title);
-    vl0->addWidget(version);
-    vl0->addWidget(copyright);
-    vl0->addWidget(line);
+      QVBoxLayout * vl1 = new QVBoxLayout();
+      vl1->addWidget(icon);
 
-    QVBoxLayout * vl1 = new QVBoxLayout();
-    vl1->addWidget(icon);
-    vl1->addItem(new QSpacerItem(20, 20, QSizePolicy::Minimum, QSizePolicy::Expanding));
-
+   
+      QHBoxLayout * hl0 = new QHBoxLayout();
+      hl0->addLayout(vl1);
+      hl0->addLayout(vl0);
+      widget1->setLayout(hl0);
 
 
-    QTextBrowser* content = new QTextBrowser();
-    content->setOpenExternalLinks(true);
-    content->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-    content->setStyleSheet("QTextBrowser { background:transparent }");
-    content->setFrameShape(QFrame::NoFrame);
-    content->setMinimumHeight(150);
-
-
-    content->setHtml("<dl>"
-                     "<dt><b>About</b></dt>"
-                     "<dd>"
-                     "Yarock is Qt4 Modern Music Player designed to provide an easy and pretty music collection browser based on cover art."
-                     " It's a Free Software distributed under the <a href=\"http://www.gnu.org/licenses/gpl-3.0.en.html\">(GPLv3)</a> license."
-                     " Project is hosted on <a href=\"https://launchpad.net/yarock\">launchpad</a> repository."
-                     "</dd>"
-                     "<br>"
-                     "<dt><b>Author</b></dt>"
-                     "<dd>"
-                     "Yarock is developped by <b>Sebastien Amardeilh</b> <a href=\"mailto:sebastien.amardeilh+yarock@gmail.com\">(sebastien.amardeilh+yarock@gmail.com)</a>"
-                     "</dd>"
-                     "<br>"
-                     "<dt><b>Bugs report</b></dt>"
-                     "<dd>Please use <a href=\"https://bugs.launchpad.net/yarock\">https://bugs.launchpad.net/yarock</a> to report bugs."
-                     "</dd>"
-                     "<br>"
-                     "<dt><b>Translations</b></dt>"
-                     "<dd>Please use <a href=\"https://www.transifex.net/projects/p/yarock-translation/\">transifex</a> for translations."
-                     "</dd>"
-                     "<br>"
-                     "<dt><b>Donation</b></dt>"
-                     "<dd>If you can, please <a href=\"https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=PBL2A87TRZPH4\">donate</a> to support the development</dd>"
-                     "</dl>");
-
-     
-    vl0->addWidget(content);
+    /* ------------------ ABOUT HEADER ------------------  */    
+    int Ypos = 10;
     
-    /*      final layout   */
-    QHBoxLayout * hl0 = new QHBoxLayout();
-    hl0->addLayout(vl1);
-    hl0->addLayout(vl0);
+    CategorieGraphicItem *cat = new CategorieGraphicItem(qobject_cast<QGraphicsView*> (parentView())->viewport());
+    cat->m_name = "About";
+    cat->setPos( 0 ,Ypos);
+    addItem(cat);
     
-    main_widget->setLayout(hl0);
+    Ypos += 30;
     
     /* proxy widget */
     QGraphicsProxyWidget* proxy_widget = new QGraphicsProxyWidget( );
-    proxy_widget->setWidget( main_widget );
+    proxy_widget->setWidget( widget1 );
     proxy_widget->setSizePolicy( QSizePolicy::Expanding, QSizePolicy::Expanding );    
     
-    proxy_widget->setPos(10, 30);
+    proxy_widget->setPos(20, Ypos);
+    addItem(proxy_widget);
+
+    Ypos += widget1->height() +10;
     
     
-    /* proxy widget */
-    this->addItem(proxy_widget);
+    TextGraphicItem *text0 = new TextGraphicItem();
+    text0->setOpenExternalLinks(true);
+    text0->setHtml( QString(tr(
+                "Yarock is a Qt4 modern music player designed to provide an easy and pretty music collection browser based on cover art.<br>"
+                "It's a Free Software distributed under the <a href=\"http://www.gnu.org/licenses/gpl-3.0.en.html\">(GPLv3)</a> license.<br>"
+                            )));
+            
+    text0->setPos( 20, Ypos);
+    addItem(text0);
+    
+    Ypos += text0->height() + 10;
+    
+    /* ------------------ CONTRIBUTE ------------------  */    
+    CategorieGraphicItem *cat2 = new CategorieGraphicItem(qobject_cast<QGraphicsView*> (parentView())->viewport());
+    cat2->m_name = tr("Contribute");
+    cat2->setPos( 0 ,Ypos);
+    addItem(cat2);    
+       
+    Ypos += 35;
     
     
-    /* ajust SceneRect */
+    /* WEBSITE */    
+    TextGraphicItem *text1 = new TextGraphicItem();
+    text1->setOpenExternalLinks(true);
+    text1->setHtml( QString("Yarock %1").arg(S_WEBSITE) );
+            
+    text1->setPos( 40, Ypos);
+    addItem(text1);
+        
+    Ypos += text1->height() + 10;
+                   
+    /* AUTHOR */    
+    QGraphicsPixmapItem* pix2 = this->addPixmap( QPixmap(":/images/view-artist_18x18.png") );
+    pix2->setPos( 20, Ypos+5);
+                
+    TextGraphicItem *text2 = new TextGraphicItem();
+    text2->setOpenExternalLinks(true);
+    text2->setHtml( QString(tr("Yarock is developped by <b>Sebastien Amardeilh</b> %1")).arg(S_MAIL) );
+            
+    text2->setPos( 40, Ypos);
+    addItem(text2);
+    
+    
+    Ypos += text2->height() + 10;
+    
+    /* BUGREPORT */    
+    TextGraphicItem *text3 = new TextGraphicItem();
+    text3->setOpenExternalLinks(true);
+    text3->setHtml( QString(tr("Please use %1 to report bugs.")).arg(S_LAUNCHPAD) );
+            
+    text3->setPos( 40, Ypos);
+    addItem(text3);
+        
+    Ypos += text3->height() + 10;    
+    
+    /* Translations */    
+    QGraphicsPixmapItem* pix4 = this->addPixmap( QPixmap(":/images/rename-48x48.png").scaled(QSize(18, 18), Qt::KeepAspectRatio, Qt::SmoothTransformation) );
+    pix4->setPos( 20, Ypos+5);
+    
+    TextGraphicItem *text4 = new TextGraphicItem();
+    text4->setOpenExternalLinks(true);
+    text4->setHtml( QString(tr("Please use %1 for translations.")).arg(S_TRANSLATE) );
+            
+    text4->setPos( 40, Ypos);
+    addItem(text4);
+        
+    Ypos += text4->height() + 10;
+  
+    /* DONATE */
+    QGraphicsPixmapItem* pix5 = this->addPixmap( QPixmap(":/images/favorites-18x18.png") );
+    pix5->setPos( 20, Ypos+5);
+                
+    TextGraphicItem *text5 = new TextGraphicItem();
+    text5->setOpenExternalLinks(true);
+    text5->setHtml( QString(tr("If you can, please %1 (or %2) to support the development"))
+                    .arg(S_DONATE,S_DONATE2) );
+    text5->setPos( 40, Ypos);
+    addItem(text5);
+
+    Ypos += text5->height() + 30;    
+    
+    /* ------------------ DEBUG INFO ------------------  */    
+    CategorieGraphicItem *cat3 = new CategorieGraphicItem(qobject_cast<QGraphicsView*> (parentView())->viewport());
+    cat3->m_name = tr("Debug Information");
+    cat3->setPos( 0 ,Ypos);
+    addItem(cat3);    
+       
+    Ypos += 35;
+
+    TextGraphicItem *text6 = new TextGraphicItem();
+    text6->setHtml( QString("<b>Compiled with Qt Version</b>: %1").arg(QT_VERSION_STR) );
+    text6->setPos( 40, Ypos);
+    addItem(text6);
+ 
+    Ypos += text6->height() + 10;
+    
+    TextGraphicItem *text7 = new TextGraphicItem();
+    text7->setHtml( QString("<b>Qt Runtime Version</b>: %1").arg(qVersion()) );
+    text7->setPos( 40, Ypos);
+    addItem(text7);
+    
+    Ypos += text7->height() + 10;
+    
+    QString audio_engine_info = Engine::instance() ? 
+        Engine::instance()->name() + "-" + Engine::instance()->version() : 
+        QString("no engine");
+    
+    TextGraphicItem *text8 = new TextGraphicItem();
+    text8->setHtml( QString("<b>Audio engine</b>: %1").arg(audio_engine_info) );
+    text8->setPos( 40, Ypos);
+    addItem(text8);
+
+
+    /* ------------------ ajust SceneRect ------------------  */
     setSceneRect ( itemsBoundingRect().adjusted(0, -10, 0, 40) );
 }
 
